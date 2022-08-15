869e2598c6a0a9f4b76f24f63043043b925fe145
Walter Tronics <waltertronics02@gmail.com>
2022-08-15 19:12:54 +0100

    commenting

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.onload=()=>{
+
+//Selecting elements
 let data = document.querySelector('#data'),
 result = document.querySelector('#result'),
 output = document.querySelector('#output'),
@@ -15,25 +17,43 @@ link = document.querySelector('a');
 
 //Showing the 'To Centimeter Section'
 btn2.addEventListener('click',()=>{
+    
+    //hiding the to feet section
     sect.style.display="none";
+    //showing the to centimeter section
     sect2.style.display="block";
+    //changing the background color of the button
     btn2.style.background="#3498db";
+    //changing the background color of the link
     btn.style.background="none";
+    //changing the border color of the link
     btn.style.borderColor="#2ecc71";
+    //changing the background color of the body
     body.style.background="#3498db";
+    //adding a box shadow to the box
     link.style.boxShadow="5px 5px 7px #51afee, inset 5px 5px 7px #0b3a58";
 });
 
+
 //Returning back to the Feet and Inches Section
 btn.addEventListener('click',()=>{
+
+    //hiding the to centimeter section
     sect2.style.display="none";
+    //showing the to feet section
     sect.style.display="block"
+    //changing the background color of the button
     btn.style.background="#2ecc71";
+    //changing the border color of the link
     btn2.style.borderColor="#3498db";
+    //changing the background color of the link
     btn2.style.background="none";
+    //changing the background color of the body
     body.style.background="#2ecc71";
+    //adding a box shadow to the box
     link.style.boxShadow="5px 5px 7px #4aeb8d, inset 5px 5px 7px #094e26";
 });
+
 
 //Centimeters to Feet and Inches starts
 var button = document.querySelector('#convert');
@@ -47,6 +67,7 @@ button.addEventListener('click',()=>{
     }
 });
 //Centimeters to Feet and Inches ends
+
 
 //Feet and Inches to centimeters starts
 var button2 = document.querySelector('#convert2');
