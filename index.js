9dd6af4ae2383aeea0474e1214045e606ecd2f70
Walter Tronics <waltertronics02@gmail.com>
2022-08-15 19:20:57 +0100

    Adding comments

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ link = document.querySelector('a');
 
 //Showing the 'To Centimeter Section'
 btn2.addEventListener('click',()=>{
-    
+
     //hiding the to feet section
     sect.style.display="none";
     //showing the to centimeter section
@@ -56,13 +56,23 @@ btn.addEventListener('click',()=>{
 
 
 //Centimeters to Feet and Inches starts
+//Selecting the button
 var button = document.querySelector('#convert');
+//adding an event listener to the button
 button.addEventListener('click',()=>{
+    //getting the value from the input field
+    //if the value is not empty, then convert
     if (data.value!=="") {
+    //divide the value by 2.54 to get the value in centimeter
     let cal = data.value/2.54;
+    //converting the value to feet and inches
     output.value = Math.floor(cal/12);
+    //getting the remainder of the value
+    //fixing the value to the output field
     output2.value = (cal-12*output.value).toFixed(1);
+
     }else{
+        //if the value is empty, then show an error message
         alert("Please input a value");
     }
 });
@@ -70,14 +80,24 @@ button.addEventListener('click',()=>{
 
 
 //Feet and Inches to centimeters starts
+//Selecting the button
 var button2 = document.querySelector('#convert2');
+//Adding an event listener to the button
 button2.addEventListener('click',()=>{
+    //getting the value from the input field
+    //if both the values are not empty, then convert
     if (input.value!=="" && input2.value!=="") {
+        //multiply the value by 30.48 to get the value in centimeter
+        //converting the value to feet and inches
         let cal = (input.value * 30.48 + input2.value * 2.54).toFixed(0);
+        //fixing the value to the output field
         result.value=cal;  
+
     }else{
+        //if both the values are empty, then show an error message
         alert("Please input your values"); 
     }
+    
 });
 //Feet and Inches to Centimeters ends
 }
